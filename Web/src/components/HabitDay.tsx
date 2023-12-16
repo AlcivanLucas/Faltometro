51d29371aa2efd48e2fad498936ef03d29d94f26
GalinGdS <alcivan14lucas@gmail.com>
2023-12-16 19:14:16 -0300

    refatoração do ben 10

---
+++ Web/src/components/HabitDay.tsx
@@ -0,0 +1,57 @@
+import * as Popover from '@radix-ui/react-popover';
+import clsx from 'clsx';
+import { useState } from 'react';
+import { calculateCompletedPercentage } from '../utils/calculate-completed-percentage';
+import { HabitModal } from './HabitModal';
+
+interface HabitDayProps {
+    date: Date;
+    defaultCompleted?: number;
+    defaultAmount?: number;
+}
+
+export function HabitDay({
+    defaultCompleted = 0,
+    defaultAmount = 0,
+    date,
+}: HabitDayProps) {
+    const defaultCompletedPercentage = calculateCompletedPercentage(
+        defaultAmount,
+        defaultCompleted
+    );
+
+    const [completedPercentage, setCompletedPercentage] = useState(
+        defaultCompletedPercentage
+    );
+
+    function handleCompletedPercentage(percentage: number) {
+        setCompletedPercentage(percentage);
+    }
+
+    return (
+        <Popover.Root>
+            <Popover.Trigger
+                className={clsx(
+                    'w-10 h-10 rounded-lg border-2  ',
+                    {
+                        'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+                        'bg-red-500 border-red-400': completedPercentage > 0 && completedPercentage < 20,
+                        'bg-red-600 border-red-500': completedPercentage >= 20 && completedPercentage < 40,
+                        'bg-red-700 border-red-600': completedPercentage >= 40 && completedPercentage < 60,
+                        'bg-red-800 border-red-700': completedPercentage >= 60 && completedPercentage < 80,
+                        'bg-red-900 border-red-800': completedPercentage >= 80,
+                    }
+                )}
+            />
+            <Popover.Portal>
+                <>
+                    <HabitModal
+                        date={date}
+                        handleCompletedPercentage={handleCompletedPercentage}
+                        completedPercentage={completedPercentage}
+                    />
+                </>
+            </Popover.Portal>
+        </Popover.Root>
+    );
+}
---
+++ Web/src/components/Header.jsx
@@ -0,0 +1,64 @@
+import { Plus, X } from 'phosphor-react'
+import * as Dialog from '@radix-ui/react-dialog';
+import { useState } from 'react'
+
+import logoImage from '../assets/logo.svg'
+import { NewHabitForm } from './NewHabitForm';
+
+import { useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import Button from "./Button";
+
+
+
+export function Header() {
+    const { signout } = useAuth();
+    const navigate = useNavigate();
+    
+    return (
+        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+            <img src={logoImage} alt="" />
+
+            <Dialog.Root>
+                <Dialog.Trigger
+                    type="button"
+                    className="border border-red-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-red-300 transition-colors focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-background"
+                >
+                    <Plus size={20} className="text-red-500" />
+                    Adicionar disciplina
+                </Dialog.Trigger>
+                
+
+
+                <div>
+                
+                    <Button 
+                        Text="Sair" 
+                        onClick={() => [signout(), navigate("/")]}
+                        
+                    >
+                            Sair
+                    </Button>
+                </div>
+
+                
+                <Dialog.Portal>
+                    <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+
+                    <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" >
+                        <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-zinc-900" >
+                            <X size={24} arial-label="Fechar" />
+                        </Dialog.Close>
+
+                        <Dialog.Title className="text-3xl leading-tight font-extrabold">
+                            Criar hábito
+                        </Dialog.Title>
+
+                        <NewHabitForm />
+                    </Dialog.Content>
+                </Dialog.Portal>
+
+            </Dialog.Root>
+        </div>
+    )
+}
