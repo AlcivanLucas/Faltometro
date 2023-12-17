e57b3985b362b9a7d8aa8aa7345840bff41d23eb
GalinGdS <alcivan14lucas@gmail.com>
2023-12-17 11:47:16 -0300

    agr na home temos apenas o mês atual

--- Web/src/utils/generate-dates-from-year-beginning.ts
+++ Web/src/utils/generate-dates-from-year-beginning.ts
@@ -1,20 +1,19 @@
 import dayjs from "dayjs";
 // determina e gera o calendário que será exibido na home page
 export function generateDatesFromYearBeginning() {
-    const firstDayOfTheYear = dayjs().startOf('year') // obtem o primeiro dia do ano
+    const firstDayOfTheMonth = dayjs().startOf('month'); // obtem o primeiro dia do mês
     const today = new Date() // obtem o dia atual
 
 
     const dates = []
-    let compareDate = firstDayOfTheYear
+    let compareDate = firstDayOfTheMonth;
 
     // gera as datas, enquanto a data comparada for menor que a data atual
     while (compareDate.isBefore(today)) {
-        dates.push(compareDate.toDate())
-        compareDate = compareDate.add(1, 'day')
+        dates.push(compareDate.toDate());
+        compareDate = compareDate.add(1, 'day');
     }
 
-    return dates // terorna as datas geradas desde o primeiro dia do ano até o dia atual
+    return dates // terorna as datas geradas desde o primeiro dia do mes até o dia atual
 }
 
-// gera datas apartir do dia 01/01/2023 até o dia atual
