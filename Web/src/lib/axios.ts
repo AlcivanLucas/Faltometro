8f2469d568088c2d2ede6e6ace7bf6a1db6167cf
AlcivanLucas <alcivan14lucas@gmail.com>
2024-02-17 14:35:01 -0300

    B

--- Web/src/lib/axios.ts
+++ Web/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-     baseURL: 'http://localhost:3333' // aqui é em desenvolvimento
-    // baseURL: 'https://faltometroserver.vercel.app' // aqui é em produção
+     //  baseURL: 'http://localhost:3333'aqui é em desenvolvimento
+    baseURL: 'https://faltometroserver.vercel.app' // aqui é em produção
 })
